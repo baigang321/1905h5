6643edcd39dc969bdbb19dff7fd3f159bc1db1cc
dante <dantinr@gmail.com>
2020-01-14 05:56:26 +0000

    init

---
+++ app/Http/Controllers/User/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+		//注册
+    public function reg(){
+    	$reg_info=[
+
+    	];
+    	//请求passport 注册接口
+    	//$url='http://1905passport.com/api/user/reg';
+    }
+}
